8db3442c9c2077fb2706ac25cce4a5d5dd8384e3
aliwerdev <aliwerdev@gmail.com>
2026-08-19 15:24:57 +0500

    Remove unused methods from CardBox enum to streamline code

--- backend/enums/CardBox.php
+++ backend/enums/CardBox.php
@@ -27,13 +27,6 @@ enum CardBox: int
     };
   }
 
-  public function intervalSeconds(): ?int
-  {
-    $days = $this->intervalDays();
-
-    return $days === null ? null : $days * 86400;
-  }
-
   public function label(): string
   {
     return match ($this) {
@@ -46,71 +39,5 @@ enum CardBox: int
       self::Box7 => "7-quti",
       self::Mastered => 'O\'zlashtirilgan',
     };
-  }
-
-  public static function first(): self
-  {
-    return self::Box1;
-  }
-
-  public static function highest(): self
-  {
-    return self::Box7;
-  }
-
-  public function promote(): self
-  {
-    return self::tryFrom($this->value + 1) ?? $this;
-  }
-
-  public function demote(): self
-  {
-    return self::first();
-  }
-
-  public function next(bool $wasCorrect): self
-  {
-    return $wasCorrect ? $this->promote() : $this->demote();
-  }
-
-  public function nextReviewAt(?int $from = null): ?int
-  {
-    $seconds = $this->intervalSeconds();
-
-    return $seconds === null ? null : ($from ?? time()) + $seconds;
-  }
-
-  public function isMastered(): bool
-  {
-    return $this === self::Mastered;
-  }
-
-  public function isFirst(): bool
-  {
-    return $this === self::first();
-  }
-
-  public static function reviewable(): array
-  {
-    return array_filter(
-      self::cases(),
-      static fn(self $box) => !$box->isMastered()
-    );
-  }
-
-  public static function values(): array
-  {
-    return array_column(self::cases(), "value");
-  }
-
-  public static function options(): array
-  {
-    $options = [];
-
-    foreach (self::cases() as $case) {
-      $options[$case->value] = $case->label();
-    }
-
-    return $options;
   }
 }
